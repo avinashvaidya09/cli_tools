6b86c85cb73c2821c900ff8a09cf8799ec13d3f7
Avinash Vaidya <Avinash_vaidya@outlook.com>
2024-10-09 13:28:46 -0400

    Added LICENSE.txt for MIT License

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
     ],
     python_requires=">=3.12",
 )
